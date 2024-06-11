a691112cccbd8768aab107709e688b4fc1af8df5
toychip <dlawnsgud427@naver.com>
2024-06-11 13:54:32 +0900

    initial commit

---
+++ src/test/java/ecs/torstory/ecsguide/EcsguideApplicationTests.java
@@ -0,0 +1,13 @@
+package ecs.torstory.ecsguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
